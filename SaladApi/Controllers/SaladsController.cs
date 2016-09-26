9643bbcf337a9a4b7cd32430fa50d8a419f1172d
Christopher Lopez <chlo1259@gmail.com>
2016-09-26 20:03:59 +0200

    Restored saladscontroller after merge

--- SaladApi/Controllers/SaladsController.cs
+++ SaladApi/Controllers/SaladsController.cs
@@ -45,5 +45,45 @@ namespace SaladApi.Controllers
         {
             return Ok(_context.Salads.FirstOrDefault(s => s.Id == id));
         }
+
+        // POST api/salads
+        [HttpPost]
+        public IActionResult Post([FromBody] Salad salad)
+        {
+            if (ModelState.IsValid) {
+                _context.Salads.Add(salad);
+                _context.SaveChanges();
+                return Created($"api/salads/{salad.Id}", salad);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // PUT api/salads/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Salad updatedSalad)
+        {
+             if (ModelState.IsValid) {
+                 var salad = _context.Salads.First(s => s.Id == id);
+                 salad.Name = updatedSalad.Name;
+                 salad.Ingredients = updatedSalad.Ingredients;
+                 _context.SaveChanges();
+                 return Ok(salad);
+             }
+             return BadRequest(ModelState);
+        }
+
+        // DELETE api/salads/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var salad = _context.Salads.FirstOrDefault(s => s.Id == id);
+            if(salad != null) {
+                _context.Remove(salad);
+                _context.SaveChanges();
+                return Ok("Salad deleted");
+            }
+                        
+            return BadRequest($"Salad with Id: {id} was not found");
+        }
     }
 }
